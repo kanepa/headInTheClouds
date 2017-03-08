e818ec65ff9e6c4a6138ef8cc363bdb2fe605374
Paul Kane <kanepa1977@gmail.com>
2017-03-08 16:35:03 +0000

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn headInTheClouds.wsgi:application
--- headInTheClouds/settings.py
+++ headInTheClouds/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'dj_database_url'
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'headInTheClouds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
+
+DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
