c2254512584179c2a4886374443c593eb44304d9
Paul Kane <kanepa1977@gmail.com>
2017-03-08 19:02:36 +0000

    take out media

--- custom_storages.py
+++ custom_storages.py
@@ -6,5 +6,5 @@ class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3BotoStorage):
-    location = settings.MEDIAFILES_LOCATION
+# class MediaStorage(S3BotoStorage):
+#     location = settings.MEDIAFILES_LOCATION
