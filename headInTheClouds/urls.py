94ae85a68205307feecd9001adbfbbad388aeee1
Paul Kane <kanepa1977@gmail.com>
2017-03-09 11:49:17 +0000

    reviewed

--- headInTheClouds/urls.py
+++ headInTheClouds/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from home.views import get_index
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', get_index, ),
